1c19a3ce8984a0c3782f01511c2aaafb62dba064
RawL <tcir3234@scs.ubbcluj.ro>
2023-05-14 13:37:17 +0300

    Added Create an Order Feature for admin too.

--- ClientFX/src/main/java/app/client/gui/BackofficeController.java
+++ ClientFX/src/main/java/app/client/gui/BackofficeController.java
@@ -5,10 +5,14 @@ import app.model.Order;
 import app.services.AppException;
 import app.services.AppObserver;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -55,7 +59,23 @@ public class BackofficeController extends Controller implements Initializable {
     }
     @FXML
     public void createOrderViewAction(){
-        //TODO: IMPLEMENT LOGIC FOR SWITCHING SCENE TO DIPLAY ORDER AND ALLOW ADMIN TO CREATE NEW ORDERS
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/display-products.fxml"));
+            Parent root = loader.load();
+
+            DisplayProductsController controller = loader.getController();
+            controller.set(services, stage);
+            controller.setEmployee(employee);
+
+            services.changeObserverForClient(employee, controller);
+
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch(IOException | AppException ex){
+            Alert alert = new Alert(Alert.AlertType.ERROR, ex.getMessage());
+            alert.showAndWait();
+        }
     }
 
     @Override
